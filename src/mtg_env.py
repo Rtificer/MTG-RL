a2ee5caced8bc2d89a66d3e1749e7d8d8bf89fa4
Artificer <rtificer@gmail.com>
2024-11-10 16:15:24 -0500

    Fixed First vs. Later Turn Issues and added Draw phase

--- src/mtg_env.py
+++ src/mtg_env.py
@@ -50,18 +50,22 @@ def GameSetup(game_state):
         if random.choice([0, 1]) == 0:
             print("Jeremy goes first")
             StartingPlayer = game_state.Jeremy
+            SecondPlayer = game_state.Angel
         else:
             print("Angel goes first")
             StartingPlayer = game_state.Angel
+            SecondPlayer = game_state.Jeremy
     else:
         print("Angel chooses who starts.")
         #TODO:Replace this random.choice for RL Model Ouput on going first or second
         if random.choice([0, 1]) == 0:
             print("Angel goes first")
             StartingPlayer = game_state.Angel
+            SecondPlayer = game_state.Jeremy
         else:
             print("Jeremy goes first")
             StartingPlayer = game_state.Jeremy
+            SecondPlayer = game_state.Angel
     
     game_state.Angel.Library = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30], dtype = np.uint8)
     np.random.shuffle(game_state.Angel.Library)
@@ -96,28 +100,54 @@ def GameSetup(game_state):
             
     game_state.CurrentTurn += 1
     
-    GameRuntime(game_state, StartingPlayer)
+    GameRuntime(game_state, StartingPlayer, SecondPlayer)
 
-def GameRuntime(game_state, StartingPlayer):
+def GameRuntime(game_state, StartingPlayer, SecondPlayer):
     if game_state.CurrentTurn > 1:
         print("Not the First Turn")
-        #Untap Phase (1a)
-        #Starts at zero 1 = Tapped Status
-        StartingPlayer.Battlefield[1] = 0
+        #Untap Subphase (1a)
+        #Starts at zero because 1 = Tapped Status
+        SecondPlayer.Battlefield[1] = 0
         game_state.GamePhase = 2
-        #TODO: Pack Gamestate
-    else:
-        ActivePlayer = StartingPlayer
-        game_state.GamePhase = 2
-        #TODO: Pack Gamestate
-        #TODO: Make the RL Model and check to see if it wants to cast instants or activate abilities
+        #Upkeep Subphase (1b)
         for CardID in range(ActivePlayer.Battlefield.shape[0]):
             CardInstance = cb.get_card_by_id(CardID)
             for ability in CardInstance.abilities:
-                if ability.trigger == 'upkeep':  # Replace 'upkeep' with the appropriate trigger condition
+                if ability.trigger == 'upkeep': #check for things that trigger at the begining of the upkeep subphase
                     print(f"Activating {ability.trigger} for card ID {CardID}")
                     ability.activate()
-
+                    
+        #TODO: Pack Gamestate
+        #TODO: Make the RL Model and check to see if it wants to cast instants or activate abilities
+        
+        game_state.GamePhase = 3
+        #Draw Subphase (1c)
+        draw_cards(SecondPlayer, 1)
+        
+        #TODO: Pack Gamestate
+        #TODO: Make the RL Model and check to see if it wants to cast instants or activate abilities
+        
+        #Skip Begining of Combat Subphase bc it's unnessasary
+        game_state.GamePhase = 4
+        #Declare Attackers Subphase (2b)
+    else:
+        ActivePlayer = StartingPlayer
+        game_state.GamePhase = 2
+        #Upkeep Subphase (1b)
+        for CardID in range(ActivePlayer.Battlefield.shape[0]):
+            CardInstance = cb.get_card_by_id(CardID)
+            for ability in CardInstance.abilities:
+                if ability.trigger == 'upkeep': #check for things that trigger at the begining of the upkeep subphase
+                    print(f"Activating {ability.trigger} for card ID {CardID}")
+                    ability.activate()
+                    
+        #TODO: Pack Gamestate
+        #TODO: Make the RL Model and check to see if it wants to cast instants or activate abilities
+        
+        #Skip Draw Subphase on first turn
+        #Skip Begining of Combat Subphase bc it's unnessasary
+        game_state.GamePhase = 5
+        #Declare Attackers Subphase (2b)
 
 
 # Create the game state
