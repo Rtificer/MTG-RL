87fa4679b461a54836d52bd12c5935571b9554a8
Artificer <rtificer@gmail.com>
2024-11-12 16:34:33 -0500

    Minor Performance Improvement

--- src/mtg_env.py
+++ src/mtg_env.py
@@ -144,40 +144,41 @@ def GameRuntime(game_state, StartingPlayer):
         game_state.GamePhase = 6
         #Declare Attackers Subphase (2b)
         for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
-    
-            CardID = ActivePlayer.Battlefield[0, CardIndex]
-            CardInstance = cb.get_card_by_id(CardID)
-            #If the card is a creature that isn't tapped and does not have summoning sickness
-            if "Creature" in CardInstance.types and ActivePlayer.Battlefield[1, CardIndex] == 0 and ActivePlayer.Battlefield[2, CardIndex] == 0:
-                if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
-                    ActivePlayer.Battlefield[4, CardIndex] = 1 #Mark That Card As Attacking
-            
+            if ActivePlayer.Battlefield[1, CardIndex] == 0 and ActivePlayer.Battlefield[2, CardIndex] == 0:   #Start by only checking the thing that doesn't require indexing the Card registry
+                CardID = ActivePlayer.Battlefield[0, CardIndex]
+                CardInstance = cb.get_card_by_id(CardID)
+                #If the card is a creature that isn't tapped and does not have summoning sickness
+                if "Creature" in CardInstance.types:
+                    if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
+                        ActivePlayer.Battlefield[4, CardIndex] = 1 #Mark That Card As Attacking
+                    
         game_state.GamePhase = 7
         #Declare Defenders Subphase (2c)
         #For every Card In the Defending players library
         for CardIndex in range(InactivePlayer.Battlefield.shape[0]):
-            CardID = InactivePlayer.Battlefield[0, CardIndex]
-            CardInstance = cb.get_card_by_id(CardID)
-            #If the card is an untapped creature
-            if "Creature" in CardInstance.types and ActivePlayer.Battlefield[1, CardIndex] == 0:
-                #Go through every card in the opponents library
-                for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
-                    #If that card is attacking
-                    if ActivePlayer.Battlefield[4, CardIndex] == 1:
-                        #And the Rl model wants to defend it
-                        if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
-                            DefenderSlot = 5 #Would be 8 becuase of the 8 slots, but is 13 bc the defender slots start 6 units down, and 0 is the first index
-                            #Iterate through every defender slot on the attacking card
-                            while DefenderSlot <= 13:
-                                #Check if it's available
-                                if ActivePlayer.Battlefield[DefenderSlot, CardIndex] == 0:
-                                    #If it is mark this card as defending it (in the first available slot)
-                                    ActivePlayer.Battlefield[DefenderSlot, CardIndex] = CardID
+            if ActivePlayer.Battlefield[1, CardIndex] == 0:  #Start by only checking the thing that doesn't require indexing the Card registry
+                CardID = InactivePlayer.Battlefield[0, CardIndex]
+                CardInstance = cb.get_card_by_id(CardID)
+                #If the card is an untapped creature
+                if "Creature" in CardInstance.types:
+                    #Go through every card in the opponents library
+                    for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
+                        #If that card is attacking
+                        if ActivePlayer.Battlefield[4, CardIndex] == 1:
+                            #And the Rl model wants to defend it
+                            if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
+                                DefenderSlot = 5 #Would be 8 becuase of the 8 slots, but is 13 bc the defender slots start 6 units down, and 0 is the first index
+                                #Iterate through every defender slot on the attacking card
+                                while DefenderSlot <= 13:
+                                    #Check if it's available
+                                    if ActivePlayer.Battlefield[DefenderSlot, CardIndex] == 0:
+                                        #If it is mark this card as defending it (in the first available slot)
+                                        ActivePlayer.Battlefield[DefenderSlot, CardIndex] = CardID
+                                    else:
+                                    #If its not available try again on the next one
+                                        DefenderSlot += 1
                                 else:
-                                #If its not available try again on the next one
-                                    DefenderSlot += 1
-                            else:
-                                print("Error: Ran out of defender slots") 
+                                    print("Error: Ran out of defender slots") 
 
         #TODO: Make the RL Models decide if they wants to cast instants or activate abilities
 
@@ -208,46 +209,46 @@ def GameRuntime(game_state, StartingPlayer):
         game_state.GamePhase = 6
         #Declare Attackers Subphase (2b)
         for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
-            
-            CardID = ActivePlayer.Battlefield[0, CardIndex]
-            CardInstance = cb.get_card_by_id(CardID)
-            #If the card is a creature that isn't tapped and does not have summoning sickness
-            if "Creature" in CardInstance.types and ActivePlayer.Battlefield[1, CardIndex] == 0 and ActivePlayer.Battlefield[2, CardIndex] == 0:
-                if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
-                    ActivePlayer.Battlefield[4, CardIndex] = 1 #Mark That Card As Attacking
+            if ActivePlayer.Battlefield[1, CardIndex] == 0 and ActivePlayer.Battlefield[2, CardIndex] == 0:   #Start by only checking the thing that doesn't require indexing the Card registry
+                CardID = ActivePlayer.Battlefield[0, CardIndex]
+                CardInstance = cb.get_card_by_id(CardID)
+                #If the card is a creature that isn't tapped and does not have summoning sickness
+                if "Creature" in CardInstance.types:
+                    if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
+                        ActivePlayer.Battlefield[4, CardIndex] = 1 #Mark That Card As Attacking
                     
         game_state.GamePhase = 7
         #Declare Defenders Subphase (2c)
         #For every Card In the Defending players library
         for CardIndex in range(InactivePlayer.Battlefield.shape[0]):
-            CardID = InactivePlayer.Battlefield[0, CardIndex]
-            CardInstance = cb.get_card_by_id(CardID)
-            #If the card is an untapped creature
-            if "Creature" in CardInstance.types and ActivePlayer.Battlefield[1, CardIndex] == 0:
-                #Go through every card in the opponents library
-                for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
-                    #If that card is attacking
-                    if ActivePlayer.Battlefield[4, CardIndex] == 1:
-                        #And the Rl model wants to defend it
-                        if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
-                            DefenderSlot = 5 #Would be 8 becuase of the 8 slots, but is 13 bc the defender slots start 6 units down, and 0 is the first index
-                            #Iterate through every defender slot on the attacking card
-                            while DefenderSlot <= 13:
-                                #Check if it's available
-                                if ActivePlayer.Battlefield[DefenderSlot, CardIndex] == 0:
-                                    #If it is mark this card as defending it (in the first available slot)
-                                    ActivePlayer.Battlefield[DefenderSlot, CardIndex] = CardID
+            if ActivePlayer.Battlefield[1, CardIndex] == 0:  #Start by only checking the thing that doesn't require indexing the Card registry
+                CardID = InactivePlayer.Battlefield[0, CardIndex]
+                CardInstance = cb.get_card_by_id(CardID)
+                #If the card is an untapped creature
+                if "Creature" in CardInstance.types:
+                    #Go through every card in the opponents library
+                    for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
+                        #If that card is attacking
+                        if ActivePlayer.Battlefield[4, CardIndex] == 1:
+                            #And the Rl model wants to defend it
+                            if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
+                                DefenderSlot = 5 #Would be 8 becuase of the 8 slots, but is 13 bc the defender slots start 6 units down, and 0 is the first index
+                                #Iterate through every defender slot on the attacking card
+                                while DefenderSlot <= 13:
+                                    #Check if it's available
+                                    if ActivePlayer.Battlefield[DefenderSlot, CardIndex] == 0:
+                                        #If it is mark this card as defending it (in the first available slot)
+                                        ActivePlayer.Battlefield[DefenderSlot, CardIndex] = CardID
+                                    else:
+                                    #If its not available try again on the next one
+                                        DefenderSlot += 1
                                 else:
-                                #If its not available try again on the next one
-                                    DefenderSlot += 1
-                            else:
-                                print("Error: Ran out of defender slots") 
+                                    print("Error: Ran out of defender slots") 
 
         #TODO: Make the RL Models decide if they wants to cast instants or activate abilities
 
         game_state.GamePhase = 8
         #Combat Damadge Subphase (2d)
-        
 
 
 # Create the game state
