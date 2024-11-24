cd198c272edc9af4746d42db1784958cf0498c96
Artificer <rtificer@gmail.com>
2024-11-24 14:43:01 -0500

    Added Sacrice and Add Card To Exile Functions

--- src/Functions.py
+++ src/Functions.py
@@ -195,12 +195,47 @@ def draw_cards(player, num_cards):
         # Place the drawn card into the hand
         player.Hand[HandIndex] = DrawnCardID
 
-def sacrifice(targetplayer, target):
-    for CardIndex in range(targetplayer.Battlefield.shape[0]):
-        CardID = targetplayer.Battlefield[0, CardIndex]
-        if CardID == target:
-            targetplayer.ExileZone = np.append(targetplayer.ExileZone, CardID)
-            targetplayer.Battlefield = np.delete(targetplayer.Battlefield, CardIndex, axis = 1)
+def AddCardToExileZone(player, CardID):
+    try:
+        #player.ExileZone[0, :] == 0 is a boolean array of the first row (card IDs) of the battlefield. True when a card slot does not contain a card
+        #argmax my beloved will scan through this array and output the index of the first occurence of true, in this case
+        Index = np.argmax(player.ExileZone[0, :] == 0)
+    except:
+        raise OverflowError(f"No space in {player}'s ExileZone")
+        
+    # If all card slots are full, np.argmax returns 0, which could be incorrect.
+    if player.ExileZone[0, Index] != 0:
+        raise OverflowError(f"No space in {player}'s ExileZone")
+    
+    player.ExileZone[:, Index] = CardID
+
+def sacrifice(player, CardID, Index):
+    if CardID != None:
+        # Find the column index of the TargetCardID
+        Index = np.argmax(player.Battlefield[0, :] == CardID)
+        
+        # Check if the card exists in the battlefield
+        if player.Battlefield[0, Index] != CardID:
+            raise ValueError(f"Card ID {CardID} not found in battlefield.")
+        
+    elif Index != None:
+        #Check if the index is valid
+        if Index > player.Battlefield.shape[1] or Index < 0:
+            raise ValueError(f"Target index {Index} is not within the battlefield bounds.")
+        else:
+            CardID = player.Battlefield[:, Index]
+    else:
+    
+        raise ValueError("No Index or CardID given.")
+    
+    
+    #Removing Card By Index is Faster
+    RemoveCardFromBattlefield(player, None, Index)
+    AddCardToExileZone(player, CardID)
+    
+    
+    
+
             
 def destroy(targetplayer, target):
     for CardIndex in range(targetplayer.Battlefield.shape[0]):
