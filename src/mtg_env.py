b6603ac0f8a1008988919d8d81a85329d771a0e0
Artificer <rtificer@gmail.com>
2024-12-05 19:57:27 -0500

    Changed arrays to custom class that is more efficent  - Other changes to mtg_env.py that are not permanent

--- src/mtg_env.py
+++ src/mtg_env.py
@@ -6,6 +6,8 @@ from Functions import draw_cards
 from Functions import register_decks
 from Functions import register_cards
 from Functions import DefineSettings
+from utils.utils import WriteIndexArray
+from utils.utils import TwoDimenionalWriteIndexArray
 
 register_cards()
 gameSettings = DefineSettings()
@@ -18,7 +20,7 @@ class GameState:
         #0 for setup and 1-12 for each phase and subphase
         self.GamePhase = np.uint8(20)
         self.Players = []
-        self.TurnOrder = np.array([0] * gameSettings["PlayerCount"], dtype = np.uint8)
+        self.TurnOrder = []
         
 class Player:
     
@@ -29,11 +31,11 @@ class Player:
         self.ID = ID
         self.Deck = None
         self.Life = np.uint8(20)
-        self.ManaPool = np.array([0] * 7, dtype = np.uint8) #White, Blue, Black, Red, Green, White, Colorless
-        self.Hand = np.array([0] * gameSettings["MaxHandSize"], dtype = np.uint8)
-        self.Library = np.array([0] * gameSettings["MaxLibrarySize"], dtype = np.uint8)
-        self.Graveyard = np.array([0] * gameSettings["MaxGraveyardSize"], dtype = np.uint8)
-        self.ExileZone = np.array([0] * gameSettings["MaxExileZoneSize"], dtype = np.uint8)
+        self.ManaPool = WriteIndexArray(6) #White, Blue, Black, Red, Green, Colorless
+        self.Hand = WriteIndexArray(gameSettings["MaxHandSize"])
+        self.Library = WriteIndexArray(gameSettings["MaxLibrarySize"])
+        self.Graveyard = WriteIndexArray(gameSettings["MaxGraveyardSize"])
+        self.ExileZone = WriteIndexArray(gameSettings["MaxExileZoneSize"])
         #Card ID
         #Tapped
         #Summoning Sickness
@@ -65,7 +67,7 @@ def SetupGameState():
 
         i += 1
 
-def GameSetup(game_state):
+def GameSetup(game_state, StartingLibraries):
 
     #Shuffle Player Libraries
     for player in game_state.Players:
@@ -80,15 +82,13 @@ def GameSetup(game_state):
     StartingPlayer = random.choice(game_state.Players)
     
     #Establish Turn Order
-    Index = 0
     for Player in game_state.Players:
         if Player != StartingPlayer:
-            Index += 1
-            game_state.TurnOrder[Index] = Player.ID
+            game_state.TurnOrder.append(Player)
             
-    np.random.shuffle(game_state.TurnOrder)
+    random.shuffle(game_state.TurnOrder)
     
-    game_state.TurnOrder[0] = StartingPlayer.ID
+    game_state.TurnOrder.insert(0, StartingPlayer)
 
     for Player in game_state.Players:
         draw_cards(Player, 7)
@@ -112,20 +112,25 @@ def GameSetup(game_state):
     GameRuntime(game_state, StartingPlayer)
 
 def GameRuntime(game_state, StartingPlayer):
-    if game_state.CurrentTurn > 1:
-        if ActivePlayer == game_state.Angel:
-            ActivePlayer = game_state.Jeremy
-            InactivePlayer = game_state.Angel
+    if game_state.CurrentTurn < 1:
+        ActivePlayer = StartingPlayer
+        ActivePlayerIndex = 0
+        print("First turn not implemented yet")
+    else:
+        if ActivePlayerIndex < len(game_state.TurnOrder):
+            ActivePlayerIndex += 1
         else:
-            ActivePlayer = game_state.Angel
-            InactivePlayer = game_state.Jeremy
+            ActivePlayerIndex = 0
         print("Not the First Turn")
         #Untap Subphase (1a)
         #Starts at zero because 1 = Tapped Status
         ActivePlayer.Battlefield[1] = 0
         game_state.GamePhase = 2
         #Upkeep Subphase (1b)
-        for CardID in ActivePlayer.Battlefield[0]:
+        #Creates a boolean array that is true when the first value in each column is not zero, and false otherwise. Than uses this mask to mask the first row the battlefield, returning the card ID's of all cards in the battlefield.
+        OccupiedBattlefieldSlotsCardIDs = ActivePlayer.Battlefield[0, ActivePlayer.Battlefield[0, :] != 0]
+        
+        for CardID in OccupiedBattlefieldSlotsCardIDs:
             CardInstance = cb.get_card_by_id(CardID)
             for ability in CardInstance.abilities:
                 if ability.trigger == 'upkeep': #check for things that trigger at the begining of the upkeep subphase
@@ -143,142 +148,22 @@ def GameRuntime(game_state, StartingPlayer):
         game_state.GamePhase = 4
         #Main Phase (2)
         #TODO: Make the RL Model responsible for ActivePlayer decide if it wants to play sorceries, instants, creatures, artifacts, enchantments,  and planeswalkers, and activate abilities. They can play 1 land during this phase
-        #TODO: Make the RL Model responsible for the other Player decide if it wants to cast instants or activate the abilities
+        #TODO: Make the RL Model responsible for the other Players decide if they wants to cast instants or activate the abilities
         #Skip Begining of Combat Subphase bc it's unnessasary
         game_state.GamePhase = 6
         #Declare Attackers Subphase (2b)
-        for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
-            if ActivePlayer.Battlefield[1, CardIndex] == 0 and ActivePlayer.Battlefield[2, CardIndex] == 0:   #Start by only checking the thing that doesn't require indexing the Card registry
-                CardID = ActivePlayer.Battlefield[0, CardIndex]
-                CardInstance = cb.get_card_by_id(CardID)
-                #If the card is a creature that isn't tapped and does not have summoning sickness
-                if "Creature" in CardInstance.types:
-                    if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
-                        ActivePlayer.Battlefield[6, CardIndex] = 1 #Mark That Card As Attacking
-                    
-        game_state.GamePhase = 7
-        #Declare Defenders Subphase (2c)
-        #For every Card In the Defending players library
-        for CardIndex in range(InactivePlayer.Battlefield.shape[0]):
-            if ActivePlayer.Battlefield[1, CardIndex] == 0:  #Start by only checking the thing that doesn't require indexing the Card registry
-                CardID = InactivePlayer.Battlefield[0, CardIndex]
-                CardInstance = cb.get_card_by_id(CardID)
-                #If the card is an untapped creature
-                if "Creature" in CardInstance.types:
-                    #Go through every card in the opponents library
-                    for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
-                        #If that card is attacking
-                        if ActivePlayer.Battlefield[6, CardIndex] == 1:
-                            #And the Rl model wants to defend it
-                            if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
-                                DefenderSlot = 7 #Would be 8 becuase of the 8 slots, but is 15 bc the defender slots start 8 units down, and 0 is the first index
-                                #Iterate through every defender slot on the attacking card
-                                while DefenderSlot <= 15:
-                                    #Check if it's available
-                                    if ActivePlayer.Battlefield[DefenderSlot, CardIndex] == 0:
-                                        #If it is mark this card as defending it (in the first available slot)
-                                        ActivePlayer.Battlefield[DefenderSlot, CardIndex] = CardID
-                                    else:
-                                    #If its not available try again on the next one
-                                        DefenderSlot += 1
-                                else:
-                                    print("Error: Ran out of defender slots") 
-
-        #TODO: Make the RL Models decide if they wants to cast instants or activate abilities
-
-    else:
-        ActivePlayer = StartingPlayer
-        if ActivePlayer == game_state.Angel:
-            InactivePlayer = game_state.Angel
-        else:
-            InactivePlayer = game_state.Jeremy
-            
-        game_state.GamePhase = 2
-        #Upkeep Subphase (1b)
-        for CardID in ActivePlayer.Battlefield[0]:
-            CardInstance = cb.get_card_by_id(CardID)
-            for ability in CardInstance.abilities:
-                if ability.trigger == 'upkeep': #check for things that trigger at the begining of the upkeep subphase
-                    print(f"Activating {ability.trigger} for card ID {CardID}")
-                    ability.activate()
-                    
-        #TODO: Make the RL Models decide if they wants to cast instants or activate abilities
         
-        #Skip Draw Subphase on first turn
-        game_state.GamePhase = 4
-        #Main Phase (2)
-        #TODO: Make the RL Model responsible for ActivePlayer decide if it wants to play sorceries, instants, creatures, artifacts, enchantments,  and planeswalkers, and activate abilities. They can play 1 land during this phase
-        #TODO: Make the RL Model responsible for the other Player decide if it wants to cast instants or activate the abilities
-        #Skip Begining of Combat Subphase bc it's unnessasary
-        game_state.GamePhase = 6
-        #Declare Attackers Subphase (2b)
-        for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
+        OccupiedBattlefieldSlotsMask = ActivePlayer.Battlefield[0, :] != 0
+        for CardIndex in np.where(OccupiedBattlefieldSlotsMask)[0]:
             if ActivePlayer.Battlefield[1, CardIndex] == 0 and ActivePlayer.Battlefield[2, CardIndex] == 0:   #Start by only checking the thing that doesn't require indexing the Card registry
-                CardID = ActivePlayer.Battlefield[0, CardIndex]
-                CardInstance = cb.get_card_by_id(CardID)
+                CardInstance = cb.get_card_by_id(CardIndex[0])
                 #If the card is a creature that isn't tapped and does not have summoning sickness
                 if "Creature" in CardInstance.types:
-                    if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
-                        ActivePlayer.Battlefield[6, CardIndex] = 1 #Mark That Card As Attacking
+                    #TODO: Mark That Card As Attacking
                     
         game_state.GamePhase = 7
         #Declare Defenders Subphase (2c)
-        #For every Card In the Defending players library
-        for CardIndex in range(InactivePlayer.Battlefield.shape[0]):
-            if ActivePlayer.Battlefield[1, CardIndex] == 0:  #Start by only checking the thing that doesn't require indexing the Card registry
-                CardID = InactivePlayer.Battlefield[0, CardIndex]
-                CardInstance = cb.get_card_by_id(CardID)
-                #If the card is an untapped creature
-                if "Creature" in CardInstance.types:
-                    #Go through every card in the opponents library
-                    for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
-                        #If that card is attacking
-                        if ActivePlayer.Battlefield[6, CardIndex] == 1:
-                            #And the Rl model wants to defend it
-                            if random.choice([0, 1]) == 0: #Randomness is placeholder for RL decision. Delete when implemented
-                                DefenderSlot = 7 #Would be 8 becuase of the 8 slots, but is 15 bc the defender slots start 8 units down, and 0 is the first index
-                                #Iterate through every defender slot on the attacking card
-                                while DefenderSlot <= 15:
-                                    #Check if it's available
-                                    if ActivePlayer.Battlefield[DefenderSlot, CardIndex] == 0:
-                                        #If it is mark this card as defending it (in the first available slot)
-                                        ActivePlayer.Battlefield[DefenderSlot, CardIndex] = CardID
-                                        break
-                                    else:
-                                    #If its not available try again on the next one
-                                        DefenderSlot += 1
-                                else:
-                                    print("Error: Ran out of defender slots") 
 
-        #TODO: Make the RL Models decide if they wants to cast instants or activate abilities
-
-        game_state.GamePhase = 8
-        #Combat Damage Subphase (2d)
-        for CardIndex in range(ActivePlayer.Battlefield.shape[0]):
-            if ActivePlayer.Battlefield[6, CardIndex] == 1: #for every creature that is attacking
-                CardID = ActivePlayer.Battlefield[0, CardIndex]
-                CardInstance = cb.get_card_by_id(CardID)
-                #Get the defenders into a list for the purpose of randomization in place of the RL Model
-                DefenderIndexes = []
-                DefendersChecked = 0
-                DefendersQuantity = 0
-                #Iterate through every defender slot on the attacking card
-                while DefendersChecked <= 8:
-                    #Check if theirs a defender
-                    if ActivePlayer.Battlefield[DefendersChecked + 7, CardIndex] != 0:
-                        #Get their ID
-                        DefenderID = ActivePlayer.Battlefield[DefendersChecked + 7, CardIndex]
-                        #Get the Index of that defender in the opponents battlefield
-                        for DefenderCardIndex in range(InactivePlayer.Battlefield.shape[0]):
-                            #If they find that card id
-                            if InactivePlayer.Battlefield[0, CardIndex] == DefenderID:
-                                #Add that Card ID to a list of defender indexes
-                                DefenderIndexes.append(DefenderCardIndex)
-                                DefendersQuantity += 1
-                    else:
-                        break
-                random.shuffle(DefenderIndexes)
-                #TODO: Replace this randomization and the placing of the defenders into a list with a decision from the RL Model
             
                     
                     
--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,112 @@
+import numpy as np
+
 def obtainpositiveinteger(prompt):
     while True:
         try:
             value = int(input(prompt))
-            if value > 0:
+            
+            #For a power of 2 the leading bit will always be 1 followed by zeros.
+            #Subtracting one leads to 0111...111111
+            #The & operation ands all the bits together, but because subtracting one returns 0111...111, none of the bits between the two numbers will be the same
+            #Thus the & operation yields a false in the case that value is a power of two.
+            if value > 0 and (value & (value - 1)) == 0:
                 return value
             else:
-                print(f"{value} is not a valid input. Please input a positive integer.")
+                print(f"{value} is not a valid input. Please input a positive power of two.")
         except ValueError:
             print("Please input an integer value.")
+            
+class WriteIndexArray:
+    def __init__(self, size):
+        
+        self.array = np.zeros(size, np.uint16)
+        self.start = 0
+        self.size = size
+    
+    def add(self, data):
+        
+        if self.start < self.size:
+
+            #Write to first empty index            
+            self.array[self.start] = data
+            #Increment start counter
+            self.start += 1
+            
+        else:
+            
+            raise ValueError("No Space Left in 1DWriteIndexArray")
+        
+    def remove(self, index):
+        if index is None:
+            
+            raise ValueError("No Index given.")            
+        
+        #Check if the index is valid            
+        if index >= self.start or index < 0:
+            
+            raise ValueError(f"Target index {index} is not within the 1DWriteIndexArray bounds.")           
+            
+        #If index is the last value containing data
+        if index == self.start - 1:
+            
+            #There is no need to shift, just set that column to 0
+            self.array[index] = 0
+        
+        else:
+            
+            # Shift all data left from the target index    
+            self.array[index:self.start-1] = self.array[index+1:self.start]
+        
+            # Reset the last column to zero
+            self.array[self.start-1] = 0
+        
+        
+class TwoDimenionalWriteIndexArray:
+    def __init__(self, rows, columns):
+        
+        self.array = np.zeros((rows, columns), np.uint16)
+        self.start = 0
+        self.totalcolumns = columns
+    
+    def add(self, data):
+        if len(data) != self.array.shape[0]:
+                
+                raise ValueError(f"Data must have {self.array.shape[0]} rows, but got {len(data)}")
+            
+        if self.start < self.totalcolumns:
+            
+            #Write to first empty index
+            self.array[:, self.start] = data
+            #Increment start counter
+            self.start += 1
+                
+        else:
+            
+            raise ValueError("No Space Left in 2DWriteIndexArray")
+        
+    def remove(self, index):
+        if index is None:
+            
+            raise ValueError("No Index or given.")            
+        
+        #Check if the index is valid            
+        if index >= self.start or index < 0:
+            
+            raise ValueError(f"Target index {index} is not within the 2DWriteIndexArray bounds.")           
+            
+        #If index is the last value containing data
+        if index == self.start - 1:
+            
+            #There is no need to shift, just set that column to 0
+            self.array[:, index] = 0
+        
+        else:
+            
+            # Shift all data left from the target index    
+            self.array[:, index:self.start-1] = self.array[:, index+1:self.start]
+        
+            # Reset the last column to zero
+            self.array[:, self.start-1] = 0
+        
+        #Decrement start counter
+        self.start -= 1
