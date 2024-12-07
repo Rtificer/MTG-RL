2d1cbf766cfa2d92d01a5a436a370cda77e70958
Artificer <rtificer@gmail.com>
2024-12-07 15:46:09 -0500

    Created Class for probabilty vector array input - Optimized existing array classes to use np.roll

--- utils/utils.py
+++ utils/utils.py
@@ -5,14 +5,10 @@ def obtainpositiveinteger(prompt):
         try:
             value = int(input(prompt))
             
-            #For a power of 2 the leading bit will always be 1 followed by zeros.
-            #Subtracting one leads to 0111...111111
-            #The & operation ands all the bits together, but because subtracting one returns 0111...111, none of the bits between the two numbers will be the same
-            #Thus the & operation yields a false in the case that value is a power of two.
-            if value > 0 and (value & (value - 1)) == 0:
+            if value > 0:
                 return value
             else:
-                print(f"{value} is not a valid input. Please input a positive power of two.")
+                print(f"{value} is not a valid input. Please input a positive value.")
         except ValueError:
             print("Please input an integer value.")
             
@@ -25,7 +21,7 @@ class WriteIndexArray:
     
     def add(self, data):
         
-        if self.start < self.size:
+        if self.start <= self.size-1:
 
             #Write to first empty index            
             self.array[self.start] = data
@@ -55,7 +51,7 @@ class WriteIndexArray:
         else:
             
             # Shift all data left from the target index    
-            self.array[index:self.start-1] = self.array[index+1:self.start]
+            self.array[index:self.start] = np.roll(self.array[index:self.start], shift=-1, axis=1)
         
             # Reset the last column to zero
             self.array[self.start-1] = 0
@@ -69,14 +65,14 @@ class TwoDimenionalWriteIndexArray:
         
         self.array = np.zeros((rows, columns), np.uint16)
         self.start = 0
-        self.totalcolumns = columns
+        self.maxindex = columns-1
     
     def add(self, data):
         if len(data) != self.array.shape[0]:
                 
                 raise ValueError(f"Data must have {self.array.shape[0]} rows, but got {len(data)}")
             
-        if self.start < self.totalcolumns:
+        if self.start <= self.maxindex:
             
             #Write to first empty index
             self.array[:, self.start] = data
@@ -90,7 +86,7 @@ class TwoDimenionalWriteIndexArray:
     def remove(self, index):
         if index is None:
             
-            raise ValueError("No Index or given.")            
+            raise ValueError("No Index given.")            
         
         #Check if the index is valid            
         if index >= self.start or index < 0:
@@ -106,10 +102,92 @@ class TwoDimenionalWriteIndexArray:
         else:
             
             # Shift all data left from the target index    
-            self.array[:, index:self.start-1] = self.array[:, index+1:self.start]
+            self.array[:, index:self.start] = np.roll(self.array[:, index:self.start], shift=-1, axis=1)
         
             # Reset the last column to zero
             self.array[:, self.start-1] = 0
         
         #Decrement start counter
         self.start -= 1
+        
+    def query(self, index):
+        
+        if index < 0 or index >= self.start:
+            
+            raise ValueError(f"Index {index} is not within 2DStartIndexArray bounds.")
+        
+        return self.array[:, index]
+        
+class TwoDimensionalStartIndexArray:
+    def __init__(self, rows, columns):
+        
+        self.array = np.zeros((rows, columns), np.uint16)
+        self.start = 0
+        self.maxindex = columns-1
+    
+    def add(self, data, index):
+        if index is None:
+            
+            raise ValueError("No Index given.")
+        
+        #Check if the index is valid            
+        if index >= self.maxindex or index < 0:
+            
+            raise ValueError(f"Target index {index} is not within the 2DStartIndexArray bounds.")
+        
+        if index > self.start:
+            
+            raise ValueError(f"Target index {index} is within unoccuped space.")
+              
+        if len(data) != self.array.shape[0]:
+                
+            raise ValueError(f"Data must have {self.array.shape[0]} rows, but got {len(data)}")
+            
+        if self.start > self.maxindex:
+
+            raise ValueError("No Space Left in 2DStartIndexArray")
+               
+        if index < self.start:
+            
+            #Roll data from index to 1 
+            self.array[:, index:self.start+1] = np.roll(self.array[:, index:self.start+1], shift=1, axis=1)
+            
+            #The only case this is false is if we are trying to write to the bottom of the deck, in which case no shifting is nessasary
+            
+        self.array[:, index] = data
+        self.start += 1
+        
+    def remove(self, index):
+        if index is None:
+            
+            raise ValueError("No Index given.")            
+        
+        #Check if the index is valid            
+        if index >= self.start or index < 0:
+            
+            raise ValueError(f"Target index {index} is not within the 2DStartIndexArray bounds.")           
+            
+        #If index is the last value containing data
+        if index == self.start - 1:
+            
+            #There is no need to shift, just set that column to 0
+            self.array[:, index] = 0
+        
+        else:
+            
+            # Shift all data left from the target index    
+            self.array[:, index:self.start] = np.roll(self.array[:, index:self.start], shift=-1, axis=1)
+        
+            # Reset the last column to zero
+            self.array[:, self.start-1] = 0
+        
+        #Decrement start counter
+        self.start -= 1
+
+    def query(self, index):
+        
+        if index < 0 or index >= self.start:
+            
+            raise ValueError(f"Index {index} is not within 2DStartIndexArray bounds.")
+        
+        return self.array[:, index]
